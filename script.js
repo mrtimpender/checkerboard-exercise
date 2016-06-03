4f91241877cf5c62832f7c8204150e3a0b6e761a
Tim Pender <mrtimpender@gmail.com>
2016-06-03 10:01:58 -0600

    randomColors completed

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+var randoColor = function(){
+  var rgb1 = Math.floor(Math.random()*256);
+  var rgb2 = Math.floor(Math.random()*256);
+  var rgb3 = Math.floor(Math.random()*256);
+  var rgb = "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")";
+  return rgb;
+}
+
 var checkerboard = function() {
     document.body.style.margin = "0";
-    for (i=0; i<45; i++){
+    for (i=0; i<64; i++){
       var div = document.createElement('div');
+      var rgb = "";
       div.style.width = "11.1%";
       div.style.cssFloat = "left";
-      div.style.paddingBottom = "11.1%"
-      if (i % 2 == 0) {
-        div.style.backgroundColor = "#FF0000";
-      } else {
-        div.style.backgroundColor = "#000000";
-      }
+      div.style.paddingBottom = "11.1%";
+      div.style.backgroundColor = randoColor();
       document.body.appendChild(div);
     }
 }
